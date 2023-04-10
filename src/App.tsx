740c57a3416782fdc646627b5383fc46d3661ee0
Gamze-Mise <gamzegm8@gmail.com>
2023-04-10 10:36:12 +0300

    apiden veriyi çekip ekrana basma

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { API } from "../api/notusapi";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const [state1, setState1] = useState(null as any);
+  async function getApi() {
+    // ?lastItem=0&limit=100&network=dev
+    let blocks = await API.get("/blocks", {
+      params: {
+        lastItem: 0,
+        limit: 100,
+        network: "dev",
+      },
+    });
+    setState1(blocks.data);
+  }
+  useEffect(() => {
+    getApi();
+  }, []);
 
-  return <div className="App">Helllooooğğ</div>;
+  return (
+    <div className="App">
+      {state1 &&
+        state1.map((value: any) => {
+          return <div>{value.row_no}</div>;
+        })}
+    </div>
+  );
 }
-
-export default App;
